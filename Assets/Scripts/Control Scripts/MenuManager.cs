264e4091c3b97a706d0f55e4245f40ee1c70cd2d
Matthew Chan <matthew.a.chan@gmail.com>
2017-03-31 16:33:16 -0700

    Add prototype main menu

---
+++ Assets/Scripts/Control Scripts/MenuManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+    UnityEngine.UI.Button m_SinglePlayBtn;
+    UnityEngine.UI.Button m_CooperativeBtn;
+    UnityEngine.UI.Button m_DeathMatchBtn;
+    UnityEngine.UI.Button m_InstructionsBtn;
+    UnityEngine.UI.Button[] buttons;
+
+    // Use this for initialization
+    void Start () {
+        buttons = new UnityEngine.UI.Button[4];
+        buttons[0] = m_SinglePlayBtn;
+        buttons[1] = m_CooperativeBtn;
+        buttons[2] = m_DeathMatchBtn;
+        buttons[3] = m_InstructionsBtn;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadSinglePlay() {
+        Debug.Log("single play");
+        
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void LoadCooperative() {
+        Debug.Log("co-op");
+    }
+
+    public void LoadDeathMatch() {
+        Debug.Log("dm");
+    }
+
+    public void LoadInstructions() {
+        Debug.Log("instructions");
+    }
+}
